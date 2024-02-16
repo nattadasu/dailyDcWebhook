d9d8e4713b37422f818268589d4f111363b08e63
Sultan Iskandar Maulana <sultan.ridita@gmail.com>
2024-02-17 02:53:19 +0700

    Fix nekomimi boy to use ZeroChan

--- classes/zerochan.py
+++ classes/zerochan.py
@@ -10,47 +10,34 @@ ua = UserAgent(browsers=['edge', 'firefox', 'chrome'])
 
 
 class ZeroChan:
-    def __init__(self):
+    def __init__(self, is_girl: bool = True):
         config = load(open("config.yaml", "r", encoding="utf-8").read())
         uastr = ua.random
-        self.webhook = config["daily_images"]["webhooks"]["nekomimi_girl"]
-        self.headers = {
-            "Content-Type": "application/json",
-            "User-Agent": uastr
-        }
-        self.zerochan_url = "https://www.zerochan.net/Nekomimi,Female?json"
+        whook = config["daily_images"]["webhooks"]
+        self.webhook = whook["nekomimi_girl"] if is_girl else whook["nekomimi_boy"]
+        self.headers = { "Content-Type": "application/json", "User-Agent": uastr }
+        tags = ["Nekomimi", "Solo", "Female" if is_girl else "Male"]
+        tags_str = ",".join(tags)
+        self.zerochan_url = f"https://www.zerochan.net/{tags_str}?json"
         self.zerochan_default_source = "None"
+        self.img = "Furina.full.4040497.png" if is_girl else r"Chat.Noir.%28Ladybug%29.full.4025125.jpg"
         self.embed = {
-            "title": "Your daily cat girl image is here!",
+            "title": f"Your daily cat {'girl' if is_girl else 'boy'} image is here!",
             "color": 0x87387F,
             "author": {
                 "name": "ZeroChan",
-                "icon_url": "https://static.zerochan.net/Nahida.full.3803289.jpg"
+                "icon_url": f"https://static.zerochan.net/{self.img}"
             },
-            "footer": {
-                "text": "Powered by github:nattadasu/dailyDcWebhook"
-            },
+            "footer": {"text": "Powered by github:nattadasu/dailyDcWebhook"},
             "timestamp": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.000Z'),
             "fields": [
-                {
-                    "name": "Image Source",
-                    "value": self.zerochan_default_source,
-                    "inline": True
-                },
-                {
-                    "name": "Source Title/Artist",
-                    "value": "",
-                    "inline": True
-                },
-                {
-                    "name": "ID",
-                    "value": "",
-                    "inline": True
-                }
+                {"name": "Image Source", "value": self.zerochan_default_source, "inline": True},
+                {"name": "Source Title/Artist", "value": "", "inline": True},
+                {"name": "ID", "value": "", "inline": True}
             ]
         }
 
-    def invoke_catgirls(self):
+    def invoke_img(self):
         # Get image metadata from ZeroChan API
         zerochan_data = requests.get(
             self.zerochan_url, headers=self.headers).text
@@ -72,38 +59,23 @@ class ZeroChan:
 
         # Get image source information
         source = meta_data.get("source", None) or self.zerochan_default_source
+        id_val = f"[`{random_id}`](https://zerochan.net/{random_id})"
 
         # Update the embed with the image information
         self.embed.update({
             "description": f"**Tags**\n{tags}",
             "image": {"url": img_url},
             "fields": [
-                {
-                    "name": "Image Source",
-                    "value": source,
-                    "inline": True
-                },
-                {
-                    "name": "Source Title/Artist",
-                    "value": meta_data["primary"],
-                    "inline": True
-                },
-                {
-                    "name": "ID",
-                    "value": f"[`{random_id}`](https://zerochan.net/{random_id})",
-                    "inline": True
-                }
+                {"name": "Image Source","value": source,"inline": True},
+                {"name": "Source Title/Artist", "value": meta_data["primary"], "inline": True},
+                {"name": "ID", "value": id_val, "inline": True }
             ]
         })
 
         # Send the webhook
-        discord_message = {
-            "content": "",
-            "username": "ZeroChan",
-            "avatar_url": "https://static.zerochan.net/Nahida.full.3803289.jpg",
-            "embeds": [self.embed]
-        }
+        discord_message = { "content": "", 
+                            "username": "ZeroChan",
+                            "avatar_url": f"https://static.zerochan.net/{self.img}",
+                            "embeds": [self.embed] }
 
-        requests.post(self.webhook, data=json.dumps(discord_message), headers={
-            "Content-Type": "application/json"
-        })
+        requests.post(self.webhook, data=json.dumps(discord_message), headers={"Content-Type": "application/json"})
--- extensions/dailies.py
+++ extensions/dailies.py
@@ -6,14 +6,16 @@ from classes.zerochan import ZeroChan
 def daily_images():
     """Invoke all daily images classes."""
     try:
-        CatBoys().invoke_catboys()
+        # CatBoys().invoke_catboys()
+        ZeroChan(False).invoke_img()
     except Exception as e:
-        print(f'CB: {e}')
+        # print(f'CB: {e}')
+        print(f"ZCB: {e}")
 
     try:
-        ZeroChan().invoke_catgirls()
+        ZeroChan().invoke_img()
     except Exception as e:
-        print(f'ZC: {e}')
+        print(f'ZCG: {e}')
 
     try:
         TheCatApi().invoke_cats()
